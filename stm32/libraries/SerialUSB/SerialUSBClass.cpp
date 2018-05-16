6eb26339438b3444f212b4ebd72610b66ed31dd1
arpruss <arpruss@gmail.com>
2018-05-15 21:36:18 -0500

    put delay into Serial.begin()

--- stm32/libraries/SerialUSB/SerialUSBClass.cpp
+++ stm32/libraries/SerialUSB/SerialUSBClass.cpp
@@ -16,10 +16,12 @@
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
+#include <Arduino.h>
 #include "SerialUSBClass.h"
 #include "mUSB.h"
 
 void SerialUSBClass::begin(uint32_t baud, uint8_t disconnectPin) {
+  delay(200);
   setUSBDisconnectPin(disconnectPin);
   setUSBBaudRate(baud);
   mUSBInit();
